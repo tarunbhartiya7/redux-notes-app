63b42432c4969b1e61ea7b2fb31fd611d1435841
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2021-10-11 07:40:58 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Notes from './components/Notes'
+import NewNote from './components/NewNote'
+
+const App = () => {
+
+  return (
+    <div>
+      <NewNote />
+      <Notes  />
+    </div>
+  )
+}
+
+export default App
