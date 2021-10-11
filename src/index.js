7990a6982c367ff6ccbdf1d7397119a758b5aa90
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2021-10-11 19:12:48 +0530

    cleanup

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom'
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import noteReducer, { createNote } from './reducers/noteReducer'
-import filterReducer, { filterChange } from './reducers/filterReducer'
+import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
 
 const reducer = combineReducers({
   notes: noteReducer,
